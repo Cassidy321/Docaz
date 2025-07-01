157643864c0df8b10ffd65e4402200c0be2b4367
cassidy <cassidyng06@gmail.com>
2025-07-01 16:54:49 +0200

    ajout route pour page vérification email

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import NewPostPage from "@/pages/NewPostPage";
 import PostDetailsPage from "@/pages/PostDetailsPage";
 import SessionManager from "./SessionManager";
 import MyPostsPage from "@/pages/UserPosts";
+import VerificationEmailPage from "@/pages/VerificationEmailPage";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/connexion" element={<LoginPage />} />
             <Route path="/inscription" element={<RegisterPage />} />
+            <Route path="/verification-email" element={<VerificationEmailPage />} />
             <Route path="/annonce/creation" element={<NewPostPage />} />
             <Route path="/annonce/:id" element={<PostDetailsPage />} />
             <Route path="/mes-annonces" element={<MyPostsPage />} />
